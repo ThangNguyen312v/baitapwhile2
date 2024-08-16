b93c418e83642c2cd5b88c57f7b380812a6e9feb
ThangNguyen312v <tatthang112113@gmail.com>
2024-08-16 21:27:54 +0700

    baitap

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace baitapwhile2{
+    class Program{
+        static void Main(string[] Args){
+            Console.WriteLine("Nhap a:");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("nhap b");
+            int b = Int32.Parse(Console.ReadLine());
+
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            if(a == 0 || b == 0){
+                Console.WriteLine("khong co UCLN");
+            }
+             while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+                }
+            Console.WriteLine("Greatest common factor: " + a);
+            
+
+        }
+    }
+}
